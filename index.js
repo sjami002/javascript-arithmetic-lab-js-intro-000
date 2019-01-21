0f8bc6e50a5b81667b8901824d691a187e78e3df
Shoptorshi Jamil <sjami002@gmail.com>
2019-01-21 15:30:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+ function inc(n) {
+  return n+1;
+}
+
+function dec(n) {
+ return n-1;
+}
+
+function makeInt(n) {
+   var n = 2;
+    parseInt(n, 10);
+}
+
+function makeInt(n) {
+    parseInt('n', 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat("2.222");
+}
+
+function preserveDecimal(n) {
+  parseFloat("hey");
+}
